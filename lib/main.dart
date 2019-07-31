8a6e46f72f0b79aed2d5901d7fbc63dcac5ffc05
Alex <awangardowie@gmail.com>
2019-07-31 20:21:09 +0200

    W49 splitting the app into widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,16 +13,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final  questions = const [
-    {'questionText': 'What\'s your favorite color?',
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
       'answers': ['Black', 'Red', 'Green', 'White'],
     },
-
-    {'questionText': 'What\'s your favorite animal?',
+    {
+      'questionText': 'What\'s your favorite animal?',
       'answers': ['Rabbit', 'Snake', 'Horse', 'Dog'],
     },
-
-    {'questionText': 'Who\'s your favorite instructor?',
+    {
+      'questionText': 'Who\'s your favorite instructor?',
       'answers': ['Arturito', 'Rasiak', 'Tadzio', 'Norek'],
     },
   ];
@@ -30,15 +31,13 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   void _answerQuestion() {
-
-    
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
 
     print(_questionIndex);
-    if (_questionIndex < questions.length){
-        print('We have more questions!');
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
     } else {
       print('No more questions!');
     }
@@ -46,23 +45,19 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first App'),
           backgroundColor: Colors.black,
         ),
-        body: _questionIndex < questions.length ? Column(       // x ? y : z    jesli x jest true to zrob Column, jesli nie to Center
-          children: [
-            Question(
-              questions[_questionIndex] ['questionText']
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ) : Center(child: Text('You did it'),) ,
+        body: _questionIndex < _questions.length
+            ? Quiz(                                 //widget ten jest okreslony w quiz.dart, by bylo bardziej czytelnie
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
       ),
     );
   }
